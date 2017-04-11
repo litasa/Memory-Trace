0645735340e90b70ba8ed1ea3007cd9b85d4755e
Jakob Ruhl <jakob.ruhl@autodesk.com>
2017-04-11 15:21:52 +0200

    Fixed small bug related to merging

--- tool/server/server.js
+++ tool/server/server.js
@@ -2,11 +2,7 @@ const net = require('net');
 const fs = require('fs');
 const stream = require('stream');
 const ipcRenderer = require('electron').ipcRenderer
-<<<<<<< HEAD
 const send_to = require('../util/send_to.js')
-=======
-const sendTo = require('../util/sendTo.js')
->>>>>>> event_struct_system
 const moment = require('moment');
 
 var internal_socket;
@@ -16,11 +12,7 @@ var pipe = true;
 
 var external_server = net.createServer(function(socket) {
   sockets.push(socket);
-<<<<<<< HEAD
   send_to.Chart('connection-established')
-=======
-  sendTo.Chart('connection-established')
->>>>>>> event_struct_system
   console.log("connectin established")
   //var unmodified_stream = fs.createWriteStream("./previous_traces/test_" + time + ".db");
   var unmodified_stream = fs.createWriteStream("./previous_traces/" + moment().format('YYYY-MM-DD_HH-mm-ss') + ".mtrace");
@@ -32,11 +24,7 @@ var external_server = net.createServer(function(socket) {
     socket.pipe(unmodified_stream);
 
     socket.on('close', function(data) {
-<<<<<<< HEAD
       send_to.Chart('connection-closed')
-=======
-      sendTo.Chart('connection-closed')
->>>>>>> event_struct_system
       console.log("closed connection")
     })
     
@@ -51,33 +39,21 @@ var external_server = net.createServer(function(socket) {
     socket.on('error', function(error) {
       var msg = error.name + " " + error.message;
       data = { msg: msg}
-<<<<<<< HEAD
       send_to.Chart('external-error', data);
-=======
-      sendTo.Chart('external-error', data);
->>>>>>> event_struct_system
     })
 
 }).listen(8181);
 
 external_server.on('listening', function() {
   require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-<<<<<<< HEAD
     send_to.Chart('server-init', {address: add, port: external_server.address().port})
-=======
-    sendTo.Chart('server-init', {address: add, port: external_server.address().port})
->>>>>>> event_struct_system
   });
 })
 
 external_server.on('error', function(error) {
   var msg = error.name + " " + error.message;
   data = { msg: msg}
-<<<<<<< HEAD
   send_to.Chart('external-error', data);
-=======
-  sendTo.Chart('external-error', data);
->>>>>>> event_struct_system
 })
 
 ipcRenderer.on('internal-server-address', function(event, data) {
